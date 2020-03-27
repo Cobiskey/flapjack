471e633a8acacd76edc47dd8e27a7999a5663b20
Jaren <jarenn123@gmail.com>
2020-03-26 23:58:58 -0700

    Added idle animation that flips on movement

--- Flapjack/src/Actors/Player.gd
+++ Flapjack/src/Actors/Player.gd
@@ -7,8 +7,17 @@ func _physics_process(delta):
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	set_sprite_direction()
 	if is_on_floor():
 		_jumpcount = 0
+
+
+func set_sprite_direction():
+	if _velocity.x > 0:
+		get_node("AnimatedSprite").set_flip_h(false)
+	if _velocity.x < 0:
+		get_node("AnimatedSprite").set_flip_h(true)
+
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -16,13 +25,16 @@ func get_direction() -> Vector2:
 		get_y_direction()
 	)
 
+
 func get_y_direction() -> float:
 	# return -1 if we want player to jump else return 1 for falling
 	if Input.is_action_just_pressed("jump"):
+		print("space was pressed")
 		if _jumpcount <= 1:
 			_jumpcount += 1
 			return -1.0
 	return 1.0
+
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
