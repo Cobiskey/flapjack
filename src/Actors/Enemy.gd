34c7a3b27cf5aa317e11e943f3260621b02d7274
Cobiskey <cobiskey@protonmail.com>
2020-03-28 16:47:02 -0700

    Added skeleton for... skeleton. Sprite has idle state, works as test enemy. Sprite flips direction when changing direciton.

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -9,3 +9,10 @@ func _physics_process(delta):
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+	set_sprite_direction()
+
+func set_sprite_direction():
+	if _velocity.x < 0:
+		get_node("AnimatedSprite").set_flip_h(false)
+	if _velocity.x > 0:
+		get_node("AnimatedSprite").set_flip_h(true)
